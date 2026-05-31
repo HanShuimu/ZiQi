c69f5c430ebd687b198b5158e13c281fec35d8aa
hansm <527903556@qq,com>
2026-05-31 21:48:49 +0800

    Remove unused hover time import

--- src/features/spectrogramViewer/SpectrogramView.tsx
+++ src/features/spectrogramViewer/SpectrogramView.tsx
@@ -28,7 +28,6 @@ import {
 } from "./spectrogramViewport";
 import { SpectrogramTimelineNavigator } from "../../capabilities/timelineViewport";
 import {
-  formatPreciseTimeLabel,
   formatPreciseTimeWithMilliseconds,
   getPitchHoverStateFromPoint,
   getPitchLaneCssProperties
